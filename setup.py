4362fe0e15571f3764120003a0b302f70164729e
Chrisebeling <chris.ebeling.93@gmail.com>
2019-10-07 16:20:12 +1100

    Version increment. Added project structure files.

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(name='nba_stats',
+      version='0.1.1',
+      description='Contains functions used to create, manage and use a database of nba statistics.',
+      long_description=readme,
+      author='Chris Ebeling',
+      author_email='chris.ebeling.93@gmail.com',
+      classifiers=['Developer Status::3 - Alpha',
+                   'Programming Language :: Python :: 3.7',
+                   'Environment :: Win32 (MS Windows)',
+                   'License :: OSI Approved :: MIT License'],
+      license=license,
+      packages=find_packages())
